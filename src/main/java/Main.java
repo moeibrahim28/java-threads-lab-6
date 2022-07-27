8ca2dbf3520f34a7f14df69d329ebab7d1ccfb34
Moe <m.ibrahim0228@gmail.com>
2022-07-27 11:42:43 -0400

    isPrime functionality implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newCachedThreadPool();
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
-            // submit tasks to your executor
+            executor.submit(new PrimeLogger(num));
         }
+
     }
 }
 
@@ -21,8 +23,21 @@ class PrimeLogger implements Runnable {
         this.num = num;
     }
 
+    private boolean isAPrimeNumber() {
+
+        if (num < 2) return false;
+        if (num % 2 == 0 && num>2) return false;
+        for (long i = 2; i < num; ++i) {
+            if (this.num % i == 0) return false;
+        }
+        return true;
+    }
+
     @Override
     public void run() {
-        // print num if it is prime
+        boolean isPrime = isAPrimeNumber();
+        if (isPrime) {
+            System.out.println(num + " " + isPrime);
+        }
     }
 }
